2053be41e87be4d2e30d064794fc0a0fab4b164b
AaronM18 <aaron@pop-os.localdomain>
2020-02-09 00:15:17 -0600

    refactorization to class

--- bifid-cipher.py
+++ bifid-cipher.py
@@ -9,66 +9,74 @@ def getInput(lines = []):
     lines.append(line.strip('\n'))
   return lines
 
-TABLEAU = [
-  ['E', 'N', 'C', 'R', 'Y'],
-  ['P', 'T', 'A', 'B', 'D'],
-  ['F', 'G', 'H', 'I', 'K'],
-  ['L', 'M', 'O', 'Q', 'S'],
-  ['U', 'V', 'W', 'X', 'Z'],
-]
+class BifidCipher:
+  searchInTableau = lambda self, letter : next( (col, row.index(letter)) for col, row in enumerate(self.tableau) if letter in row )
 
-searchInTableau = lambda letter : next( (col, row.index(letter)) for col, row in enumerate(TABLEAU) if letter in row )
+  def __init__(self, tableau = None):
+    self.tableau = tableau
 
-def encrypt(message):
-  tuples = []
-  message = message.replace(" ", "")
+  def encrypt(self, message):
+    tuples = []
+    message = message.replace(" ", "")
 
-  # searches letter in matrix
-  for letter in message: 
-    tuples.append( searchInTableau(letter) )
+    # searches letter in matrix
+    for letter in message: 
+      tuples.append( self.searchInTableau(letter) )
 
-  row1, row2 = [], []
-  index = 0 # index of coordinate in pair
-  # Order new pairs in two lists, list of rows & list of cols
-  for coordinate in tuples:
-    row1.append(coordinate[0])
-    row2.append(coordinate[1])
-  
-  pairs = row1 + row2 # concatenate lists
+    row1, row2 = [], []
+    index = 0 # index of coordinate in pair
+    # Order new pairs in two lists, list of rows & list of cols
+    for coordinate in tuples:
+      row1.append(coordinate[0])
+      row2.append(coordinate[1])
     
-  string = '' # map pairs to new letter and append to string
-  for i in range(0, len(pairs)-1, 2):
-    newPair = pairs[i:i+2]
-    string += TABLEAU[newPair[0]][newPair[1]]
+    pairs = row1 + row2 # concatenate lists
+      
+    string = '' # map pairs to new letter and append to string
+    for i in range(0, len(pairs)-1, 2):
+      newPair = pairs[i:i+2]
+      string += self.tableau[newPair[0]][newPair[1]]
 
-  return string
+    return string
 
-def decrypt(cypherText):
-  tuples = []
-  flatten = lambda l: [item for sublist in l for item in sublist]
+  def decrypt(self, cypherText):
+    tuples = []
+    flatten = lambda l: [item for sublist in l for item in sublist]
 
-  # searches letter in matrix
-  for letter in cypherText: 
-    tuples.append( list(searchInTableau(letter)) )
-  tuples = flatten(tuples)
+    # searches letter in matrix
+    for letter in cypherText: 
+      tuples.append( list(self.searchInTableau(letter)) )
+    tuples = flatten(tuples)
 
-  half = len(tuples)//2
-  row1, row2 = tuples[:half], tuples[half:]
+    half = len(tuples)//2
+    row1, row2 = tuples[:half], tuples[half:]
 
-  message = ''
-  for tupleNumber in range(half):
-    message += TABLEAU[row1[tupleNumber]][row2[tupleNumber]]
-  
-  return message
-
+    message = ''
+    for tupleNumber in range(half):
+      message += self.tableau[row1[tupleNumber]][row2[tupleNumber]]
+    
+    return message
 
 def main():
+
+  TABLEAU = [
+    ['E', 'N', 'C', 'R', 'Y'],
+    ['P', 'T', 'A', 'B', 'D'],
+    ['F', 'G', 'H', 'I', 'K'],
+    ['L', 'M', 'O', 'Q', 'S'],
+    ['U', 'V', 'W', 'X', 'Z'],
+  ]
+
+  bifid = BifidCipher(TABLEAU)
+
   inputLines = getInput()
 
   if inputLines[0] == 'ENCRYPT':
-    res = encrypt(inputLines[1])
+    res = bifid.encrypt(inputLines[1])
+
   elif inputLines[0] == 'DECRYPT':
-    res = decrypt(inputLines[1])
+    res = bifid.decrypt(inputLines[1])
+    
   else:
     print('COMMAND ERROR')
 
